2edea284f875feef28c822d17b680dbf53a97bec
Sam Teel <sjkteel@hotmail.com>
2023-10-18 11:38:15 -0400

    header section version 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,11 @@ function Header() {
     return (
         <section className='header'>
             <h1 id="siteHeader">Welcome to Saint-Isidores</h1>
-            <h2 id="siteSubHeader">Protector of Monks</h2>
             <img id='isidoreImage' src={Isidore} alt="painting of Saint Isidore as Archbishop of Saville" ></img>
-            <p id="headerBio">Known as the Last Scholar of the Ancient World, St. Isidore is the patron saint of programmers, the Internet and students. He is also commonly depicted as being surrounded by bees or standing near bee hives. This is due to the story that as a young child, Isidore's father saw young Isidore attracting bees with a mouthful of honey. This was a sign of Isidores future as a symbolic beehive of knowledge. </p>
+            <section className='headerBio'>
+                <h2 id="siteSubHeader">Protector of Monks</h2>
+                <p id="headerBio">Known as the Last Scholar of the Ancient World, St. Isidore is the patron saint of programmers, the Internet and students. He is also commonly depicted as being surrounded by bees or standing near bee hives. This is due to the story that as a young child, Isidore's father saw young Isidore attracting bees with a mouthful of honey. This was a sign of Isidores future as a symbolic beehive of knowledge. </p>
+            </section>
         </section>
     )
 }
