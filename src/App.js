e94491413714e3acf5467462f67e13ff7a6e45b2
Sam Teel <sjkteel@hotmail.com>
2023-10-18 11:43:38 -0400

    added navbar basics

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from "./components/Header";
+import Navbar from './components/Navbar';
 import Bio from './components/Bio'
 
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Bio/>
+      <Header />
+      <Navbar />
+      <Bio />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,16 @@
 import React from 'react';//imports react functionality
+
+function Navbar (props){
+    const {setNewItem} = props;
+    return (
+        <section className='navbar'>
+            <a href ="#" id ="homeAnchor" onClick={()=>  setNewItem("Home")}>Home</a>
+            <a href ="#" id ="bioAnchor" onClick={()=>  setNewItem("Bio")}>About Us</a>
+            <a href ="#" id ="contactAnchor" onClick={()=>  setNewItem("Contact")}>Contact</a>
+            <a href ="#" id ="moreAnchor" onClick={()=>  setNewItem("More")}>Further Reading</a>
+        </section>
+
+    )
+}
+
+export default Navbar;
