f3cdbe21daa62123023458c91c23f77a23fa25e3
Sam Teel <sjkteel@hotmail.com>
2023-10-18 11:33:20 -0400

    sites first steps are taken

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
+import Bio from './components/Bio'
+
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Navbar />
-      
-      <Footer />
+      <Header/>
+      <Bio/>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';//imports react functionality
 import Isidore from '../images/Isidor_von_Sevilla.jpeg';
+import '../styles/Header.css';
 function Header() {
     return (
-        <section className='siteBio'>
+        <section className='header'>
             <h1 id="siteHeader">Welcome to Saint-Isidores</h1>
             <h2 id="siteSubHeader">Protector of Monks</h2>
-            <img src={Isidore} alt="painting of Saint Isidore as Archbishop of Saville" id='isidoreImage'></img>
+            <img id='isidoreImage' src={Isidore} alt="painting of Saint Isidore as Archbishop of Saville" ></img>
+            <p id="headerBio">Known as the Last Scholar of the Ancient World, St. Isidore is the patron saint of programmers, the Internet and students. He is also commonly depicted as being surrounded by bees or standing near bee hives. This is due to the story that as a young child, Isidore's father saw young Isidore attracting bees with a mouthful of honey. This was a sign of Isidores future as a symbolic beehive of knowledge. </p>
         </section>
     )
 }
